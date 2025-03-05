609eecb2740d12e8986284ca7dcd38b322dd0b18
zohaibhassan9 <zabaidafashion@gmail.com>
2025-03-05 21:34:49 +0500

    Complete JavaScript for the counter app by the interest of a user

--- counter.js
+++ counter.js
@@ -1,45 +1,45 @@
-const counter = document.getElementById('counter');
-const increaseBtn = document.getElementById('increase');
-const decreaseBtn = document.getElementById('decrease');
-const resetBtn = document.getElementById('reset');
-
-
-let count = 0;
-
-
-increaseBtn.addEventListener('click', ()=>{
-count++;
-counter.textContent = count;
-});
-
-decreaseBtn.addEventListener('click', ()=>{
-
-    count--;
-    counter.textContent = count;
-})
-
-resetBtn.addEventListener('click', ()=>{
-    count = 0;
-    counter.textContent = count;
-})
-// const counterBtn = document.getElementById('counter');
+// const counter = document.getElementById('counter');
 // const increaseBtn = document.getElementById('increase');
 // const decreaseBtn = document.getElementById('decrease');
 // const resetBtn = document.getElementById('reset');
 
 
-// let count  = 0;
+// let count = 0;
 
-// increaseBtn.addEventListner('click', ()=>{
-//     count++;
-//     counterBtn.textContent = count;
+
+// increaseBtn.addEventListener('click', ()=>{
+// count++;
+// counter.textContent = count;
 // });
-// decreaseBtn.addEventListner('click', ()=>{
+
+// decreaseBtn.addEventListener('click', ()=>{
+
 //     count--;
-//     counterBtn.textContent = count;
-// });
+//     counter.textContent = count;
+// })
 
-// resetBtn.addEventListner('click', ()=>{
+// resetBtn.addEventListener('click', ()=>{
 //     count = 0;
-//     counterBtn.textContent = count;
-// });
+//     counter.textContent = count;
+// })
+const counterBtn = document.getElementById('counter');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const resetBtn = document.getElementById('reset');
+
+
+let count  = 0;
+
+increaseBtn.addEventListener('click', ()=>{
+    count++;
+    counterBtn.textContent = count;
+});
+decreaseBtn.addEventListener('click', ()=>{
+    count--;
+    counterBtn.textContent = count;
+});
+
+resetBtn.addEventListener('click', ()=>{
+    count = 0;
+    counterBtn.textContent = count;
+});
